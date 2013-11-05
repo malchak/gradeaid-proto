2ca2f86e62af2a024fa149eefad85fe7047eae55
malchak <michaelmalchak@gmail.com>
2013-11-05 12:17:54 -0800

    application layout

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
 App = Ember.Application.create();
 
+App.AplicationAdapter = DS.FixtureAdapter.extend();
+
+App.Student = DS.Model.extend({
+	firstName: DS.attr('string'),
+	lastName: DS.attr('string'),
+	fullName: function(){
+		return this.get('firstName') + " " + this.get('lastName');
+	}
+});
+
+App.Student.FIXTURES = [
+	{
+		id: 1,
+		firstName: 'Harry',
+		lastName: 'Potter'
+	},
+	{
+		id: 2,
+		firstName: 'Ronald',
+		lastName: 'Weesley'
+	}
+]
+
 App.Router.map(function() {
   
 });
