da5cdbb3664825a87c7f09a3e3833e6057678e29
malchak <michaelmalchak@gmail.com>
2013-11-08 20:24:36 -0800

    create hasMany relationships

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ App = Ember.Application.create();
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 App.Student = DS.Model.extend({
+	assignments: DS.hasMany('assigment'),
 	firstName: DS.attr('string'),
 	lastName: DS.attr('string'),
 	assignments: DS.attr('string'),
@@ -11,28 +12,32 @@ App.Student = DS.Model.extend({
 	}).property('firstName', 'lastName')
 });
 
+App.Assignment = DS.Model.extend({
+	students: DS.hasMany('student'),
+	subject: DS.attr('string'),
+	type: DS.attr('string'),
+	title: DS.attr('string'),
+	pointsEarned: DS.attr('number'),
+	pointsTotal: DS.attr('number')
+});
+
 App.Student.FIXTURES = [
 	{
 		id: 1,
 		firstName: 'Harry',
 		lastName: 'Potter',
-		assignments: [
-			{
-				id: 1,
-				subject: 'Math',
-				type: 'Quiz',
-				title: 'Take Home Test',
-				points: 5,
-				outOf: 10
-			}
-		]
 	},
 	{
 		id: 2,
 		firstName: 'Ronald',
 		lastName: 'Weesley'
 	}
+];
+
+App.Assignment.Fixtures = [
+
 ]
+
 
 App.Router.map(function() {
   this.resource('students', function(){
